d599a48b0e8b1f27f52d1f484c3ccf95bfb1b392
Ismail Kamel <ikamel@uwm.edu>
2021-10-07 23:29:23 -0500

    Update IsmailKamel-BusAdm531-JSApp.js

--- IsmailKamel-BusAdm531-JSApp.js
+++ IsmailKamel-BusAdm531-JSApp.js
@@ -1,8 +1,47 @@
-const ingredientsEndpoint
+const jsmodule = require('./IsmailKamel-BusAdm531-JSModule')
+let recipeList = [];
+let ingredientLIst = [];
+const ingredientsEndpoint = '/api/ingredients'
+const recipeEndpoint = '/api/recipes'
+const recipeName = '/api/recipes:name'
+
+
+function getRecipes(){
+    jsmodule.getData(recipeEndpoint)
+    .then(response => {
+        response.forEach(name => {
+            recipeList.push(jsmodule.Recipe(item.name))
+        });
+        console.log(recipes);
+        }).catch(function(error){
+        console.log(error);
+        })
+    }
+
+    function getIngredients(){
+        jsmodule.getData(ingredientEndpoint)
+        .then(response => {
+            response.forEach(name => {
+                ingredientList.push(jsmodule.Recipe(item.name))
+            });
+            console.log(recipes);
+            }).catch(function(error){
+            console.log(error);
+            })
+        }
+
+    function getRecipe(){
+    jsmodule.calcMaxPPG('Drifter')
+        .then(response => {
+            console.log('Max PPG of ', recipeName, ' is ', calcMaxPPG);
+        }).catch(function(error){
+            console.log(error);
+            })
+        }
+
 
 
 getRecipes();
-getaRecipe('Drifter');
 getIngredients();
 calcMaxPPG('Drifter')
-console.log('this prints first')
+ 
